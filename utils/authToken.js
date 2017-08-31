b93fb16a7ae321dbb0b5291b6ab46a11a9de7899
emmanuel <eolaojo@yahoo.com>
2017-08-30 20:10:38 -0400

    added comments, changed alias to username in createToken

--- utils/authToken.js
+++ utils/authToken.js
@@ -11,6 +11,16 @@ let http = require("./HttpStats");
 
 let moduleId = "authToken";
 
+/**
+ * Checks that a user has a valid token
+ * i.e. is logged in
+ *
+ * @param req request
+ * @param res response
+ * @param next next middleware
+ *
+ * @returns {Promise.<*>}
+ */
 async function checkToken(req, res, next){
   let respondErr = response.failure(res, moduleId);
   let authToken = req.get(config.authToken);
@@ -26,10 +36,17 @@ async function checkToken(req, res, next){
   }
 }
 
+/**
+ * Creates a token from a User's details
+ *
+ * @param user the user
+ *
+ * @returns {Promise.<*>}
+ */
 async function createToken(user){
-  let {_id, alias} = user;
+  let {_id, username} = user;
 
-  return await jwt.signAsync({_id, alias}, config.secret, {expiresIn: "24h"});
+  return await jwt.signAsync({_id, username}, config.secret, {expiresIn: "24h"});
 }
 
 module.exports = {checkToken, createToken};
