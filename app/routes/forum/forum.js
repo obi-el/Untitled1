603a0cf13be818aa2d4958435d50fc1d14e6bfa3
obi-el <elobiobinna@gmail.com>
2017-09-18 14:17:54 -0400

    endpoints to get a forum and delete a forum

--- app/routes/forum/forum.js
+++ app/routes/forum/forum.js
@@ -27,6 +27,33 @@ exports.getForum = async function(req, res){
     respondErr(http.SERVER_ERROR, config.DEFAULT_ERR_MSG, err);
   }
 
+}
+
+/**
+ * Deletes forum with topic in request
+ *
+ * @param req
+ * @param res
+ * @returns {Promise.<*>}
+ */
+exports.deleteForum = async function(req,res){
+  let respond = response.success(res);
+  let respondErr = response.failure(res);
+
+  let query = Forum.findOneAndRemove({topic: req.query.topic});
+
+  try{
+    let forum = await query.exec();
+
+    if(!forum){
+      return respondErr(http.NOT_FOUND, "Forum not found");
+    }
+
+    respond(http.OK, "Forum Deleted Successfully", {user});
+  }
+  catch(err){
+    respondErr(http.SERVER_ERROR, err.message, err)
+  }
 
 }
 
--- app/routes/forum/forum.routes.js
+++ app/routes/forum/forum.routes.js
@@ -6,9 +6,12 @@
 let express = require('express');
 let {checkToken} = require("../../../utils/authToken");
 let forum = require("./forum");
+let permissions = require("./forumauth");
 let forumRouter = express.Router();
 
 
+forumRouter.get("/", forum.getForum);
 
+forumRouter.delete("/del", [checkToken, permissions.modsOnly], forum.deleteForum);
 
 module.exports = forumRouter;
