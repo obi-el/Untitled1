66a434edf08fd6966b6f1bbce64968a3335625a7
obi-el <elobiobinna@gmail.com>
2018-01-31 23:50:14 -0500

    Comment Model Creation and tests, Linter Added - [x] comment model created - [] paginated storing of comments in mongoose - [] configure comment plugins with markdown-it - [x] ESLint installed adn configured - [] add lint to pre-commit hook with husky

--- app/models/CommentModel.js
+++ app/models/CommentModel.js
@@ -4,10 +4,7 @@
 
 let Markdown = require("markdown-it");
 let mdEmoji = require("markdown-it-emoji");
-<<<<<<< Updated upstream
-=======
 let mdSubscript = require("markdown-it-sub");
->>>>>>> Stashed changes
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
@@ -16,11 +13,8 @@ let Schema = mongoose.Schema;
  */
 let CommentSchema = new Schema({
   author: {type: Schema.Types.ObjectId, ref: "Users", required: true}
-<<<<<<< Updated upstream
-=======
   , postId: {type: Schema.Types.ObjectId, required: true}
   , parentId: {type: Schema.Types.ObjectId, ref: "Users"}
->>>>>>> Stashed changes
   , text: {type: String, required: true}
   , raw_text: {type: String, required: true}
   , createdAt: Date
@@ -30,19 +24,6 @@ let CommentSchema = new Schema({
 /**
  * @todo find more markdown plugins
  */
-<<<<<<< Updated upstream
-let md = new Markdown({linkify: true, breaks: true});
-md.use(mdEmoji);
-md.disable("image");
-
-
-CommentSchema.pre("save", function(next){
-  let comment = this;
-
-  if(comment.isModified("raw_text")){
-    comment.text = md.render(comment.raw_text);
-    comment.text = comment.text.replace(/(?:\r\n|\r|\n)/g, '');
-=======
 let md = new Markdown({
   linkify: true
   , breaks: false
@@ -58,14 +39,11 @@ CommentSchema.pre("save", function (next) {
   if (comment.isModified("raw_text")) {
     comment.text = md.render(comment.raw_text);
     comment.text = comment.text.replace(/(?:\r\n|\r|\n)/g, "");
->>>>>>> Stashed changes
   }
 
   next();
 });
 
-<<<<<<< Updated upstream
+
 exports.Comments = mongoose.model("Comments", CommentSchema);
-=======
-exports.Comments = mongoose.model("Comments", CommentSchema);
->>>>>>> Stashed changes
+
